5c752e213e111e1a3327864ff1b41f5b9085bbb4
narashinhchaudhary <narashinhchaudhary426@gmail.com>
2024-03-31 16:38:53 +0530

    my changes

---
+++ styled components/src/App.js
@@ -0,0 +1,19 @@
+
+import Header1 from "./componrnts/header1";
+import NavBar from "./componrnts/Navbar";
+import Crasol from "./componrnts/Crasol";
+import Mainslaide from "./componrnts/Mainslaid";
+import Mainsec from "./componrnts/Mainsec";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <NavBar />
+      <Carousel />
+      <MainSec />
+    </>
+  );
+}
+
+export default App;
